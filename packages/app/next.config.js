e66f7ebc94c6de8ea360ab5e3ed819d920453d35
ae9is <125031666+ae9is@users.noreply.github.com>
2023-10-20 02:27:12 -0700

    fix: next.js app router removed from experiments

--- packages/app/next.config.js
+++ packages/app/next.config.js
@@ -1,8 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    appDir: true,
-  },
   reactStrictMode: true,
   // Build static export of app
   // ref: https://nextjs.org/docs/app/building-your-application/deploying/static-exports#configuration
