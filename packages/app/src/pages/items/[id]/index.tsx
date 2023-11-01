cc04b8d4e1ce32e6d8a68f47c69ee1393f109242
ae9is <125031666+ae9is@users.noreply.github.com>
2023-11-01 04:24:43 -0700

    style: format

--- packages/app/src/pages/items/[id]/index.tsx
+++ packages/app/src/pages/items/[id]/index.tsx
@@ -4,19 +4,19 @@ import { withLayout } from '../../../components/layout/withLayout'
 import { withAuth } from '../../../components/auth/withAuth'
 
 export default withLayout(function Page() {
+  const title = <h1>Item history</h1>
   const itemId = useSlug()
-
-  if (!itemId)
+  if (!itemId) {
     return (
       <>
-        <h1>Item history</h1>
+        {title}
         <p className="text-error">Error: must specify an item to view history for!</p>
       </>
     )
-
+  }
   return withAuth(
     <>
-      <h1>Item history</h1>
+      {title}
       <p>Viewing all item versions for: {itemId}</p>
       <div className="mt-4 p-2">
         <ItemVersionGrid id={itemId} />
