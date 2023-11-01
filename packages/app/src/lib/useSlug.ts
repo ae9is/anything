bbe5f16f1945b8c3d88d312ca149e24ed1f1f57b
ae9is <125031666+ae9is@users.noreply.github.com>
2023-11-01 04:12:37 -0700

    fix: useSlug hook to accept [id] or [slug] next.js paths

--- packages/app/src/lib/useSlug.ts
+++ packages/app/src/lib/useSlug.ts
@@ -1,9 +1,11 @@
-import { useRouter } from "next/router"
+import logger from 'logger'
+import { useRouter } from 'next/router'
 
 // Convenience hook to get url slug as string. If multiple slugs, just returns the last.
 export function useSlug() {
   const router = useRouter()
-  const stringOrArray = router?.query?.slug
-  const slug = (typeof stringOrArray === 'string') ? stringOrArray : stringOrArray?.at(-1)
+  const stringOrArray = router?.query?.id ?? router?.query?.slug
+  const slug = typeof stringOrArray === 'string' ? stringOrArray : stringOrArray?.at(-1)
+  logger.debug('Url slug: ', slug)
   return slug
 }
