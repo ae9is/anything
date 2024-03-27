cad254c8266d511ece430a9336551bdb2f25a600
ae9is <125031666+ae9is@users.noreply.github.com>
2024-03-26 20:57:37 -0700

    32 Migrate Lambda streams to SDKv3 (#33)
    * build: update some dependencies
    
    * refactor: convert streams to firehose lambda to typescript
    
    * refactor: migrate streams to firehose lambda to aws sdk v3
    
    * test: update streams to firehose lambda tests for migration
    
    * test: replace deprecated testing function
    
    * fix: update lambda streams to firehose provider to use node 18
    
    * fix!: transformer function binding
    
    BREAKING CHANGE: remove regex to delimiter transformer
    
    * fix: remove extra encoding of records piped to firehose, improve typing
    
    * fix: app component test rendering to use updated react api
    
    * refactor: improve streams to firehose transformer typing
    
    * fix: more typing in streams to firehose lambda
    
    * test: update tests for changes to typing
    
    * feat: add utility types to expand intellisense for type definitions
    
    * refactor: add more debug logging for handler init
    
    * refactor!: remove callbacks and simplify streams to firehose lambda [wip]
    
    BREAKING CHANGE: remove support for kinesis streams, remove aws client caching
    
    * fix: wait to retry firehose writes using promise
    
    * fix: async errors in handling streams to firehose events, remove function binds, reorder functions for readability
    
    * docs: update readme and license header
    
    * style: cleanup comment formatting
    
    * refactor: move some constants into constants file
    
    * style: format
    
    * test: fix transformer test to account for removal of transformer function binding
    
    * fix: rework routing so failures to deliver records result in all records being passed to default delivery stream

--- packages/infra/data/watch.mjs
+++ packages/infra/data/watch.mjs
@@ -1,24 +1,25 @@
 import * as esbuild from 'esbuild'
-import { glob } from 'glob'
 
-// Note: esbuild plugin glob not available yet for esbuild 0.17+.
-//  This workaround won't scan for new files while watching.
-// ref: https://github.com/waspeer/esbuild-plugin-glob
-const entryPoints = await glob('src/**/*.handler.{ts,js}')
-console.log('entryPoints:')
-entryPoints?.forEach(entry => console.log(entry))
+const entryPoints = [
+  './src/modules/stream/lambda-streams-to-firehose/handler.js',
+]
 
+// ref: https://middy.js.org/docs/best-practices/bundling/#esbuild
 // ref: https://docs.aws.amazon.com/serverless-application-model/latest/developerguide/serverless-sam-cli-using-build-typescript.html
 let ctx = await esbuild.context({
   entryPoints,
   bundle: true,
   write: true,
   outdir: '.build',
-  target: 'es2020',
+  target: 'node20',
   sourcemap: true,
   format: 'esm',
   minify: false,
   platform: 'node',
+  // --banner:js hack from https://github.com/evanw/esbuild/pull/2067
+  banner: {
+    js: "import { createRequire } from 'module';const require = createRequire(import.meta.url);",
+  },
 })
 
 await ctx.watch()
